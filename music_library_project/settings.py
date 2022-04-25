422b772c7fecc31afbea8b01829fcf72cccc1c6a
rossebell <Rosy24dr@outlook.com>
2022-04-25 14:54:21 -0400

    Renamed the Song attributes, added the installed apps in the setting.py file.

--- music_library_project/settings.py
+++ music_library_project/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    
+    'rest_framework',
+    'song'
 ]
 
 MIDDLEWARE = [
--- song/models.py
+++ song/models.py
@@ -1,10 +1,9 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
-from django.forms import CharField
+
 
 class Song(models.Model):
     title = models.CharField(max_length=200)
-    artist= models.Model(max_length=200)
-    album= models.Model(max_length=200)
-    release=models.DateField()
-    genre=models.Model(max_length=200)
+    artist = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
+    release = models.DateField()
+    genre = models.CharField(max_length=200)
