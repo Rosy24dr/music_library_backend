35859b0c64d3caa5cc98d84f5b1d1cff6243894d
rossebell <Rosy24dr@outlook.com>
2022-04-25 15:56:52 -0400

    created the song_list function in the viws file

--- song/views.py
+++ song/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def song_list(request):
+   
+   return Response('ok')
+
+
+
+# def song_by_id(GET):
+#     pass
